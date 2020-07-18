b137f1ba07bce91250daea07f8cd7db76b746856
JVasconcelosSilva <jefferson.vasconcelos1@outlook.com.br>
2020-07-18 19:31:26 -0300

    Conectando ao banco de dados

--- back/src/server.js
+++ back/src/server.js
@@ -2,3 +2,19 @@ import express from 'express';
 
 const app = express();
 app.listen(3333);
+
+// var connection = require("../database/connection");
+
+// //SELECT
+// connection.select().table("test").then(data => {
+//     console.log(data);
+//     }).catch(err => {
+//         console.log("Erro: ", err);
+//     });
+
+    // // INSERT
+    // var dados = {
+    //     nome: "Larah"
+    // }
+
+    // connection.insert(dados).into("test");
